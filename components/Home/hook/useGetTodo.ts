225f96cfc37dba17a91c95fbb8c0b76b6ecab302
진영 <bae20051018@gmail.com>
2023-01-25 23:41:39 +0900

    fix : getTodo
    투두 데이터를 받을때 프로미스로 리턴되던 문제 해결

--- components/Home/hook/useGetTodo.ts
+++ components/Home/hook/useGetTodo.ts
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 import TodoRepositories from "repositories/todo/TodoRepositories";
 
 const useGetTodo = () => {
-  const todoData = TodoRepositories.getTodo();
   const [todos, setTodos] = useState([]);
   useEffect(() => {
-    console.log("TDD", todoData);
-  });
-  // useEffect(() => {
-  //   console.log("todoData", todoData);
-  //   setTodos(todoData);
-  // }, []);
+    getTodoData();
+  }, []);
+
+  const getTodoData = async () => {
+    const todoData = await TodoRepositories.getTodo();
+    setTodos(todoData);
+  };
 
   return todos;
 };
